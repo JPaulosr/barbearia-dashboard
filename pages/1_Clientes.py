064cf8808f3f69ff8c24cd395070c0dd5127f077
JPaulosr <joaopaulosr_17@hotmail.com>
2025-06-19 23:54:56 -0300

    Update 1_Clientes.py

--- pages/1_Clientes.py
+++ pages/1_Clientes.py
@@ -4,7 +4,7 @@ import pandas as pd
 st.set_page_config(layout="wide")
 st.title("🧑‍🤝‍🧑 Clientes - Receita Total")
 
-@st.cache_data(ttl=1)
+@st.cache_data
 def carregar_dados():
     df = pd.read_excel("Modelo_Barbearia_Automatizado (10).xlsx", sheet_name="Base de Dados")
     df.columns = [str(col).strip() for col in df.columns]
