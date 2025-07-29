de964f849fd55f220df6d7099fa195953d0ed254
JPaulosr <joaopaulosr_17@hotmail.com>
2025-07-28 22:18:52 -0300

    Update 25_teste_2.py

--- pages/25_teste_2.py
+++ pages/25_teste_2.py
@@ -70,7 +70,12 @@ valores_fixos = {
 
 valores_referencia = (
     df_base[df_base["Valor"].notna() & df_base["Valor"].astype(str).str.startswith("R$")]
-    .assign(valor_num=lambda d: d["Valor"].astype(str).str.replace("R$", "", regex=True).str.replace(",", ".").astype(float))
+    .assign(valor_num=lambda d: pd.to_numeric(
+        d["Valor"].astype(str)
+        .str.replace("R\$", "", regex=True)
+        .str.replace(",", "."), errors="coerce"
+    ))
+    .dropna(subset=["valor_num"])
     .groupby("Serviço")["valor_num"]
     .mean()
     .round(2)
